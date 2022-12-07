36a20845a5d537f6f37ae6a2e425deee6a525d83
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-12-07 19:13:33 +0600

    csv to json covert functionality updated

--- src/pages/Home/FormOne/FormOne.js
+++ src/pages/Home/FormOne/FormOne.js
@@ -1,65 +1,87 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { DataContext } from '../../../context/Provider/Provider';
 
 const FormOne = () => {
+    const { setProjectName, setProjectDesc, setClientName, setContractorName, formData } = useContext(DataContext)
+    const { projectName, projectDesc, clientName, contractorName } = formData;
+
+
     return (
         <form action="" className="mx-auto mt-8 mb-0 max-w-md space-y-4">
             <div>
-                <label for="name" className="font-bold">Project Name: </label>
+                <label htmlFor="name" className="font-bold">Project Name: </label>
 
                 <div className="relative">
                     <input
+                        onChange={(e) => setProjectName(e.target.value)}
                         type="text"
                         className="w-full rounded-lg border border-black p-4 pr-12 text-sm shadow-sm mt-2"
                         placeholder="Write Project Name"
+                        defaultValue={projectName}
                     />
                 </div>
             </div>
             <div>
-                <label for="name" className="font-bold">Project Description: </label>
-
+                <label htmlFor="name" className="font-bold">Project Description: </label>
                 <div className="relative">
                     <textarea
+                        onChange={(e) => setProjectDesc(e.target.value)}
                         rows="4" cols="50"
                         type="text"
                         className="w-full rounded-lg border border-black p-4 pr-12 text-sm shadow-sm mt-2"
                         placeholder="Write Project Description"
+                        defaultValue={projectDesc}
                     />
                 </div>
             </div>
             <div>
-                <label for="name" className="font-bold">Client: </label>
+                <label htmlFor="name" className="font-bold">Client: </label>
 
                 <div className="relative">
                     <input
+                        onChange={(e) => setClientName(e.target.value)}
                         type="text"
                         className="w-full rounded-lg border border-black p-4 pr-12 text-sm shadow-sm mt-2"
                         placeholder="Write Client Name"
+                        defaultValue={clientName}
                     />
                 </div>
             </div>
             <div>
-                <label for="name" className="font-bold">Contractor: </label>
+                <label htmlFor="name" className="font-bold">Contractor: </label>
 
                 <div className="relative">
                     <input
+                        onChange={(e) => setContractorName(e.target.value)}
                         type="text"
                         className="w-full rounded-lg border border-black p-4 pr-12 text-sm shadow-sm mt-2"
                         placeholder="Write Contractor Name"
+                        defaultValue={contractorName}
                     />
                 </div>
             </div>
 
-
-            <div>
-                <Link to='/formtwo'>
-                    <span
-                        className="inline-block rounded-lg w-32 text-center bg-blue-500 px-6 py-3 text-sm font-medium text-white"
-                    >
-                        Next
-                    </span>
-                </Link>
-            </div>
+            {
+                (projectName.length === 0 || projectDesc.length === 0 || clientName.length === 0 || contractorName.length === 0) ?
+                    <div disabled>
+                        <span
+                            className="inline-block rounded-lg w-32 text-center bg-blue-200 px-6 py-3 text-sm font-medium text-white"
+                        >
+                            Next
+                        </span>
+                    </div>
+                    :
+                    <div>
+                        <Link to='/formtwo'>
+                            <span
+                                className="inline-block rounded-lg w-32 text-center bg-blue-500 px-6 py-3 text-sm font-medium text-white"
+                            >
+                                Next
+                            </span>
+                        </Link>
+                    </div>
+            }
         </form>
     );
 };
--- src/pages/Home/FormTwo/FormTwo.js
+++ src/pages/Home/FormTwo/FormTwo.js
@@ -1,60 +1,124 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { DataContext } from '../../../context/Provider/Provider';
+import { useCSVReader } from 'react-papaparse';
+
+
+
 
 const FormTwo = () => {
+    const { CSVReader } = useCSVReader();
+    const { formData } = useContext(DataContext)
+    const { projectName, projectDesc, clientName, contractorName } = formData;
+    const [csvData, setCsvData] = useState('')
+    console.log(csvData)
+    const styles = {
+        csvReader: {
+            display: 'flex',
+            flexDirection: 'row',
+            marginBottom: 10,
+        },
+        browseFile: {
+            width: '40%',
+        },
+        acceptedFile: {
+            border: '1px solid #ccc',
+            height: 45,
+            lineHeight: 2.5,
+            paddingLeft: 10,
+            width: '60%',
+        },
+        remove: {
+            borderRadius: 0,
+            padding: '0 20px',
+        },
+        progressBarBackgroundColor: {
+            backgroundColor: 'red',
+        },
+    };
+
     return (
         <form action="" className="mx-auto mt-8 mb-0 max-w-md space-y-4">
             {/* from previous input  */}
             <div>
-                <label for="name" className="font-bold">Project Name: </label>
+                <label htmlFor="name" className="font-bold">Project Name: </label>
 
                 <div className="relative">
                     <input
                         type="text"
                         className="w-full rounded-lg border border-black p-4 pr-12 text-sm shadow-sm mt-2"
-                        placeholder="Write Project Name"
+                        defaultValue={projectName} disabled
                     />
                 </div>
             </div>
             <div>
-                <label for="name" className="font-bold">Project Description: </label>
+                <label htmlFor="name" className="font-bold">Project Description: </label>
 
                 <div className="relative">
                     <textarea
                         rows="4" cols="50"
                         type="text"
                         className="w-full rounded-lg border border-black p-4 pr-12 text-sm shadow-sm mt-2"
-                        placeholder="Write Project Description"
+                        defaultValue={projectDesc} disabled
                     />
                 </div>
             </div>
             <div>
-                <label for="name" className="font-bold">Client: </label>
+                <label htmlFor="name" className="font-bold">Client: </label>
 
                 <div className="relative">
                     <input
                         type="text"
                         className="w-full rounded-lg border border-black p-4 pr-12 text-sm shadow-sm mt-2"
-                        placeholder="Write Client Name"
+                        defaultValue={clientName} disabled
                     />
                 </div>
             </div>
             <div>
-                <label for="name" className="font-bold">Contractor: </label>
+                <label htmlFor="name" className="font-bold">Contractor: </label>
 
                 <div className="relative">
                     <input
                         type="text"
                         className="w-full rounded-lg border border-black p-4 pr-12 text-sm shadow-sm mt-2"
-                        placeholder="Write Contractor Name"
+                        defaultValue={contractorName} disabled
                     />
                 </div>
             </div>
+            <hr className='border-b-2 border black' />
 
             {/* in this form  */}
+            <h4 className='text-xl'>Please Upload A CSV </h4>
+            <CSVReader
+                onUploadAccepted={(results) => {
+                    setCsvData(results)
+                }}
+            >
+                {({
+                    getRootProps,
+                    acceptedFile,
+                    ProgressBar,
+                    getRemoveFileProps,
+                }) => (
+                    <>
+                        <div style={styles.csvReader}>
+                            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 inline-flex items-center  mx-2' type='button' {...getRootProps()} style={styles.browseFile}>
+                                Browse file
+                            </button>
+                            <div style={styles.acceptedFile}>
+                                {acceptedFile && acceptedFile.name}
+                            </div>
+                            <button {...getRemoveFileProps()} style={styles.remove} className='bg-red-700 hover:bg-red-900 text-white font-bold mx-2  px-4 inline-flex items-center'>
+                                Remove
+                            </button>
+                        </div>
+                        <ProgressBar style={styles.progressBarBackgroundColor} />
+                    </>
+                )}
+            </CSVReader>
             <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
                 <div>
-                    <label for="max-x" className="font-bold">Max X: </label>
+                    <label htmlFor="max-x" className="font-bold">Max X: </label>
 
                     <div className="relative">
                         <input
@@ -66,7 +130,7 @@ const FormTwo = () => {
                     </div>
                 </div>
                 <div>
-                    <label for="min-x" className="font-bold">Min X: </label>
+                    <label htmlFor="min-x" className="font-bold">Min X: </label>
 
                     <div className="relative">
                         <input
@@ -80,7 +144,7 @@ const FormTwo = () => {
             </div>
             <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
                 <div>
-                    <label for="max-y" className="font-bold">Max Y: </label>
+                    <label htmlFor="max-y" className="font-bold">Max Y: </label>
 
                     <div className="relative">
                         <input
@@ -92,7 +156,7 @@ const FormTwo = () => {
                     </div>
                 </div>
                 <div>
-                    <label for="min-y" className="font-bold">Min Y: </label>
+                    <label htmlFor="min-y" className="font-bold">Min Y: </label>
 
                     <div className="relative">
                         <input
@@ -106,7 +170,7 @@ const FormTwo = () => {
             </div>
             <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
                 <div>
-                    <label for="max-z" className="font-bold">Max Z: </label>
+                    <label htmlFor="max-z" className="font-bold">Max Z: </label>
 
                     <div className="relative">
                         <input
@@ -118,7 +182,7 @@ const FormTwo = () => {
                     </div>
                 </div>
                 <div>
-                    <label for="min-z" className="font-bold">Min Z: </label>
+                    <label htmlFor="min-z" className="font-bold">Min Z: </label>
 
                     <div className="relative">
                         <input
@@ -132,16 +196,20 @@ const FormTwo = () => {
             </div>
 
 
-
-            <div>
-                <Link>
-                    <button
-                        type='submit'
-                        className="inline-block rounded-lg w-32 text-center bg-blue-500 px-6 py-3 text-sm font-medium text-white"
-                    >
-                        Submit
-                    </button>
+            <div className='flex items-center justify-between'>
+                <Link to='/'>
+                    <p className='inline-block rounded-lg w-32 text-center bg-blue-500 px-6 py-3 text-sm font-medium text-white'>Go Back</p>
                 </Link>
+                <div>
+                    <Link>
+                        <button
+                            type='submit'
+                            className="inline-block rounded-lg w-32 text-center bg-blue-500 px-6 py-3 text-sm font-medium text-white"
+                        >
+                            Submit
+                        </button>
+                    </Link>
+                </div>
             </div>
         </form>
     );
